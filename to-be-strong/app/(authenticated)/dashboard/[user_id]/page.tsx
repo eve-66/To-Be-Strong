a3ed928a6c12c99bcb6d0c52115e4c73be332fe3
eve-66 <is0733pi@ed.ritsumei.ac.jp>
2025-04-21 17:38:22 +0900

    eslint rule changed

--- to-be-strong/app/(authenticated)/dashboard/[user_id]/page.tsx
+++ to-be-strong/app/(authenticated)/dashboard/[user_id]/page.tsx
@@ -1,5 +1,3 @@
-import type { NextPage } from "next";
-
 const dashboard = async (props: { params: Promise<{ user_id: string }> }) => {
  const params = await props.params; 
  const user_id = params.user_id;
--- to-be-strong/eslint.config.mjs
+++ to-be-strong/eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", //! 未使用変数の警告をoff
+    },
+  },
 ];
 
 export default eslintConfig;
